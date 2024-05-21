419205c3f77e9a33c1ed8bedd9055703e727d1d8
krunal <202001442@daiict.ac.in>
2024-05-21 23:21:34 +0530

    DAY6 - Delegate, list functions, Generic methods

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    public class Program
+    {
+        delegate double doubleIt(double val);
+        static void Main(string[] args)
+        {
+            doubleIt dblIt = x => x * 2;
+            Console.WriteLine(dblIt(6));
+
+            List<int> numList = new List<int> { 1, 9, 2, 6, 3};
+
+            var evenList = numList.Where(a => a % 2 == 0).ToList();
+
+            foreach (var j in evenList) Console.WriteLine(j);
+
+            var rangeList = numList.Where(x => (x > 2) || (x < 9)).ToList();
+            foreach (var j in rangeList) Console.WriteLine(j);
+
+
+            List<int> flipList = new List<int>();
+            int i = 0;
+            Random rnd = new Random();
+            while(i < 100)
+            {
+                flipList.Add(rnd.Next(1, 3));
+                i++;
+            }
+
+            Console.WriteLine("Heads : {0}",
+                flipList.Where(a => a == 1).ToList().Count);
+            Console.WriteLine("Tails : {0}",
+                flipList.Where(a => a == 2).ToList().Count);
+
+
+            var nameList = new List<String> { "Virat", "Jordan", "Ronaldo", "Viswanth" };
+
+            var sName = nameList.Where(x => x.StartsWith("V"));
+
+            foreach(var m in sName) Console.WriteLine(m);
+
+            var oneToTen = new List<int>();
+
+            oneToTen.AddRange(Enumerable.Range(1, 10));
+
+            var squares = oneToTen.Select(x => x * x);
+
+            foreach(var l in squares) Console.WriteLine(l);
+
+
+            Console.WriteLine("---------------------------");
+
+            var listOne = new List<int>( new int[] { 1, 3, 4 } );
+            var listTwo = new List<int>( new int[] { 4, 6, 8 } );
+
+            var sumList = listOne.Zip(listTwo, (x, y) => x + y).ToList();
+
+            foreach(var sum in sumList) Console.WriteLine(sum);
+
+            Console.WriteLine("Sum: {0}",
+                sumList.Aggregate((a, b) => a + b));
+        }
+    }
+}
--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,2 +1,105 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animalList = new List<Animal>();
+
+            List<int> numList = new List<int>();
+            numList.Add(18);
+
+
+            animalList.Add(new Animal() { Name = "Doug" });
+            animalList.Add(new Animal() { Name = "Paul" });
+            animalList.Add(new Animal() { Name = "Sally" });
+
+            animalList.Insert(1, new Animal() { Name = "Steve Rogers" });
+
+            //animalList.RemoveAt(1);
+
+            Console.WriteLine("Num of Animals : {0}", animalList.Count());
+
+            foreach(Animal animal in animalList)
+            {
+                Console.WriteLine(animal.Name);
+            }
+
+            int x = 5, y = 13;
+
+            Animal.GetSum<int>(ref x, ref y);
+
+            string strx = "6", stry = "12";
+
+            Animal.GetSum<string>(ref strx, ref stry);
+
+
+            Rectangle<int> rec1 = new Rectangle<int>(20, 50);
+
+            Console.WriteLine(rec1.GetArea());
+
+            Rectangle<string> rec2 = new Rectangle<string>("30", "40");
+            Console.WriteLine(rec2.GetArea());
+
+
+            Arithmetic add, sub, addSub;
+
+            add = new Arithmetic(Add);
+            sub = new Arithmetic(Substract);
+            addSub = add + sub + add;
+
+            Console.WriteLine($"Add {6} & {10}");
+            add(6, 10);
+            Console.WriteLine($"Add & Substract {10} & {4}");
+            addSub(10, 4);
+
+        }
+
+        public struct Rectangle<T>
+        {
+            private T width;
+            private T length;
+
+            public T Width
+            {
+                get { return width; }
+                set { width = value; }
+            }
+
+            public T Length
+            {
+                get { return length; }
+                set { length = value; }
+            }
+
+            public Rectangle(T w, T l)
+            {
+                width = w;
+                length = l;
+            }
+
+            public double GetArea()
+            {
+                double dblW = Convert.ToDouble(Width);
+                double dblL = Convert.ToDouble(Length);
+
+                return dblW * dblL;
+
+            }
+        }
+
+        public delegate void Arithmetic(double num1, double num2);
+
+        public static void Add(double num1, double num2)
+        {
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+        }
+
+        public static void Substract(double num1, double num2)
+        {
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+        }
+    }
+}
