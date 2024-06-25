aece6040d7e4094caa7cbb06be9d37286670c1fb
krunal <202001442@daiict.ac.in>
2024-06-25 22:16:00 +0530

    DAY41 - Stepper, Slider and various other controls

---
+++ Phase4/MauiApp4/ControlDemo.xaml.cs
@@ -0,0 +1,59 @@
+namespace MauiApp4;
+
+public partial class ControlDemo : ContentPage
+{
+	public ControlDemo()
+	{
+		InitializeComponent();
+	}
+	private void imgBtn_Clicked(object sender, EventArgs e)
+	{
+		DisplayAlert("Alert", "You Clicked the image", "OK");
+	}
+    private void btn_Clicked(object sender, EventArgs e)
+    {
+        DisplayAlert("Alert", "You Clicked the Button", "OK");
+    }
+	private void searchBar_SearchButtonPressed(object sender, EventArgs e)
+	{
+		infoLabel.Text = $"Searching for {searchBar.Text}";
+	}
+	private void RadioButton_CheckedChanged(object sender,
+		CheckedChangedEventArgs e)
+	{
+		RadioButton sexBtn = sender as RadioButton;
+		infoLabel.Text = $"I see you are {sexBtn.Content}";
+	}
+    private void CheckBox_CheckedChanged(object sender,
+    CheckedChangedEventArgs e)
+	{
+		infoLabel.Text = $"Clicked CheckBox {e.Value}";
+	}
+	
+	private void Switch_Toggle(object sender, ToggledEventArgs e)
+	{
+		infoLabel.Text = $"Clicked Toggle {e.value}";
+	}
+	private void SwipeItem_Invoked(object sender, EventArgs e)
+	{
+        infoLabel.Text = $"Marked as Favorite";
+
+    }
+    private void SwipeItem_Invoked_1(object sender, EventArgs e)
+    {
+        infoLabel.Text = $"Marked as Delete";
+
+    }
+	private void slider_ValueChanged(object sender, ValueChangedEventArgs e)
+	{
+		sliderLabel.Text = $"People Attending {Convert.ToInt32(slider.Value)}";
+	}
+    private void stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        stepperLabel.Text = $"People Paying {Convert.ToInt32(stepper.Value)}";
+    }
+    private void entry_Completed(object sender, EventArgs e)
+    {
+        infoLabel.Text = $"Hello {entry.Text}";
+    }
+}
