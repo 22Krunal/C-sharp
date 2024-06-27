aa5b24b58180de45128815a46c336e1f17bfdd41
krunal <202001442@daiict.ac.in>
2024-06-27 20:57:42 +0530

    DAY43 - Different Methods of dataBinding and Display

--- Phase4/MauiApp5/MainPage.xaml.cs
+++ Phase4/MauiApp5/MainPage.xaml.cs
@@ -2,7 +2,6 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
 
         public MainPage()
         {
@@ -11,14 +10,24 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+            var krunal = new Player
+            {
+                Name = "Krunal Patel",
+                Info = "Has Watched All NBA Finals (1992, 1993, 1994, 1995, 1996, 1997, 1998) were Chicago Bulls Won the With Great Michel Jordan on their Side along with Scotiee Pipen, Denish Roadman, Stev Ker, Tonny Kukoch and many more with Phill Jackson as Coach"
+            };
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            /*
+            //Creates the Binding
+            Binding playerBinding = new Binding();
+            //Defines the data Sources
+            playerBinding.Source = krunal;
+            //Defines the name of the property that is shared
+            //between the source and control
+            playerBinding.Path = "Name";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            lblName.SetBinding(Label.TextProperty, playerBinding);
+            */
+            BindingContext = krunal;
         }
     }
 
